c06c3cd90043ec27f10551e4c1f6cb555fe7d836
Anurag <anuragtyagi014@gmail.com>
2024-05-09 10:53:40 +0530

    404 page not found issue fixed

--- 404.php
+++ 404.php
@@ -1,4 +1,4 @@
-<?php defined( 'ABSPATH' ) OR die( 'This script cannot be accessed directly.' );
+<?php defined('ABSPATH') or die('This script cannot be accessed directly.');
 
 /**
  * The template for displaying the 404 page
@@ -7,26 +7,26 @@
  * you should find all the needed hooks there.
  */
 
-if ( function_exists( 'us_load_template' ) ) {
 
-	us_load_template( 'templates/404' );
+if (function_exists('us_load_template')) {
 
+	us_load_template('templates/404');
 } else {
 	get_header();
-	?>
+?>
 	<main id="page-content" class="l-main">
 		<section class="l-section">
 			<div class="l-section-h i-cf">
 				<div class="page-404 align_center">
 					<?php
-					$the_content = '<h1>' . us_translate( 'Page not found' ) . '</h1>';
-					$the_content .= '<p>' . __( 'The link you followed may be broken, or the page may have been removed.', 'us' ) . '</p>';
-					echo apply_filters( 'us_404_content', $the_content );
+					$the_content = '<h1>' . us_translate('Page not found') . '</h1>';
+					$the_content .= '<p>' . __('The link you followed may be broken, or the page may have been removed.', 'us') . '</p>';
+					echo apply_filters('us_404_content', $the_content);
 					?>
 				</div>
 			</div>
 		</section>
 	</main>
-	<?php
+<?php
 	get_footer();
 }
